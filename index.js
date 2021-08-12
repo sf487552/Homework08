f62a20cc313e5fb711f610dffe02a9c76e52d991
Steve Fokas <sf487552@gmail.com>
2021-08-12 17:41:41 -0400

    added html/css pages

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const fs = require('fs');
 // const { checkPrime } = require('crypto');
 
 // generate html file path
-// const generateHtml = './Homework08/TeamProfile.html'
+const generateHtmlFilePath = './Homework08/TeamProfile.html'
 
 // array of objects that holds team members
 let teamMembers = [];
@@ -41,22 +41,20 @@ inquirer
     message:"enter manager's Office #",
   },
   {
-    name: "AddTeamMembers",
+    name: "addTeamMembers",
     type: "list",
     message: "select team members to add",
     choices: ["Engineer","Intern"]
   },
-
 ])
-.then((answers) => {
+.then(answers => {
   // create manager
   let manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
   // add to team members
   teamMembers.push(manager);
-  evaluteAddedTeamMember(answers.addTeamMember)
+  evaluteAddedTeamMember(answers.addTeamMembers);
 })
-
-.catch((error) => {
+.catch(error => {
   if (error.isTtyError) {
     // Prompt couldn't be rendered in the current environment
   } else {
@@ -66,102 +64,100 @@ inquirer
 
 function addEngineer()
 {
-  inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-      name:"engineerName",
-      type:"input",
-      message:"enter Engineer's name",
-    },
-    {
-      name:"engineerID",
-      type:"input",
-      message:"enter Engineer's employee ID#",
-    },
-    {
-      name:"engineerEmail",
-      type:"input",
-      message:"enter Engineer's Email Address",
-    },
-    {
-      name:"engineerGithub",
-      type:"input",
-      message:"enter Engineer's Github Username",
-    },
-    {
-      name: "AddTeamMembers",
-      type: "list",
-      message: "select team members to add",
-      choices: ["Engineer","Intern","Exit"]
-    },
-  
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    let engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
-    // add to array
-    teamMembers.push(Engineer);
-    evaluteAddedTeamMember(answers.addTeamMember)
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+inquirer
+.prompt([
+  /* Pass your questions in here */
+  {
+    name:"engineerName",
+    type:"input",
+    message:"enter Engineer's name",
+  },
+  {
+    name:"engineerID",
+    type:"input",
+    message:"enter Engineer's employee ID#",
+  },
+  {
+    name:"engineerEmail",
+    type:"input",
+    message:"enter Engineer's Email Address",
+  },
+  {
+    name:"engineerGithub",
+    type:"input",
+    message:"enter Engineer's Github Username",
+  },
+  {
+    name: "addTeamMembers",
+    type: "list",
+    message: "select team members to add",
+    choices: ["Engineer","Intern","Exit"]
+  },
+])
+.then(answers => {
+  // Use user feedback for... whatever!!
+  let engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+  // add to array
+  teamMembers.push(Engineer);
+  evaluteAddedTeamMember(answers.addTeamMembers)
+})
+.catch(error => {
+  if (error.isTtyError) {
+    // Prompt couldn't be rendered in the current environment
+  } else {
+    // Something else went wrong
+  }
+});
 }
 
 // call this as many times as team members added
 function addIntern()
 {
-  inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-      name:"engineerName",
-      type:"input",
-      message:"enter intern's name",
-    },
-    {
-      name:"internID",
-      type:"input",
-      message:"enter intern's employee ID#",
-    },
-    {
-      name:"internEmail",
-      type:"input",
-      message:"enter intern's Email Address",
-    },
-    {
-      name:"internSchool",
-      type:"input",
-      message:"enter intern's school",
-    },
-    {
-      name: "AddTeamMembers",
-      type: "list",
-      message: "select team members to add",
-      choices: ["Engineer","Intern","Exit"]
-    },
-  
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    let intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
-    // add to array
-    teamMembers.push(intern);
-    // evaluate additions
-    evaluteAddedTeamMember(answers.addTeamMember)
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+inquirer
+.prompt([
+  /* Pass your questions in here */
+  {
+    name:"internName",
+    type:"input",
+    message:"enter intern's name",
+  },
+  {
+    name:"internID",
+    type:"input",
+    message:"enter intern's employee ID#",
+  },
+  {
+    name:"internEmail",
+    type:"input",
+    message:"enter intern's Email Address",
+  },
+  {
+    name:"internSchool",
+    type:"input",
+    message:"enter intern's school",
+  },
+  {
+    name: "addTeamMembers",
+    type: "list",
+    message: "select team members to add",
+    choices: ["Engineer","Intern","Exit"]
+  },
+])
+.then(answers => {
+  // Use user feedback for... whatever!!
+  let intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+  // add to array
+  teamMembers.push(intern);
+  // evaluate additions
+  evaluteAddedTeamMember(answers.addTeamMembers)
+})
+.catch(error => {
+  if (error.isTtyError) {
+    // Prompt couldn't be rendered in the current environment
+  } else {
+    // Something else went wrong
+  }
+});
 }
 
 function evaluteAddedTeamMember(result)
@@ -184,5 +180,5 @@ function evaluteAddedTeamMember(result)
 function generateHtml()
 {
   // Use user feedback for... whatever!!
-  fs.writeFileSync(generateHtml,"")
+  fs.writeFileSync(generateHtmlFilePath,"")
 }
