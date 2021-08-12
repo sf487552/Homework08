94fa5fb54bf0aaf766abedde48c1dad3b89abf72
Steve Fokas <sf487552@gmail.com>
2021-08-12 12:20:54 -0400

    test

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const Intern = require('./lib/Intern');
 
 // inquirer package
 const inquirer = require("inquirer");
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+    name:"title",
+    type:"input",
+    message:"",
+    }
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+});
 
 // file system
 const fs = require('fs');
@@ -17,3 +36,5 @@ console.log(manager.getID());
 console.log(manager.getEmail());
 console.log(manager.getRole());
 console.log(manager.getOfficeNumber());
+
+// asdfafhgj
